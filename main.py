b86b4b038b6c1d747fe1e31cae040bb7a9a12403
Konstantin Konovalov <53082951+Ka-ra-siK@users.noreply.github.com>
2021-09-08 21:29:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -79,68 +79,6 @@ async def _balance(ctx, member: discord.Member = None):
             description = f"""Баланс **{member}** = **{cursor.execute("SELECT cash FROM users WHERE id = {}".format(member.id)).fetchone()[0]} :leaves:**"""
         ))
 
-# @client.command()
-# async def play(ctx, url : str):
-#     song_there = os.path.isfile("song.mp3")
-#     try:
-#         if song_there:
-#             os.remove("song.mp3")
-#     except PermissionError:
-#         await ctx.send("Wait for the current playing music to end or use the 'stop' command")
-#         return
-#
-#     voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='General')
-#     await voiceChannel.connect()
-#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-#
-#     ydl_opts = {
-#         'format': 'bestaudio/best',
-#         'postprocessors': [{
-#             'key': 'FFmpegExtractAudio',
-#             'preferredcodec': 'mp3',
-#             'preferredquality': '192',
-#         }],
-#     }
-#     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-#         ydl.download([url])
-#     for file in os.listdir("./"):
-#         if file.endswith(".mp3"):
-#             os.rename(file, "song.mp3")
-#     voice.play(discord.FFmpegPCMAudio("song.mp3"))
-#
-#
-# @client.command()
-# async def leave(ctx):
-#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-#     if voice.is_connected():
-#         await voice.disconnect()
-#     else:
-#         await ctx.send("The bot is not connected to a voice channel.")
-#
-#
-# @client.command()
-# async def pause(ctx):
-#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-#     if voice.is_playing():
-#         voice.pause()
-#     else:
-#         await ctx.send("Currently no audio is playing.")
-#
-#
-# @client.command()
-# async def resume(ctx):
-#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-#     if voice.is_paused():
-#         voice.resume()
-#     else:
-#         await ctx.send("The audio is not paused.")
-#
-#
-# @client.command()
-# async def stop(ctx):
-#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-#     voice.stop()
-
 @client.command(pass_context = True)
 async def clear(ctx, amount = 2):
     await ctx.channel.purge(limit = amount)
@@ -156,8 +94,6 @@ async def on_message(message):
     await client.process_commands(message)
     msg = message.content.lower()
 
-    #if msg in hello_list:
-      #  await message.channel.send('Привет! Как дела? Как сам?')
     if msg in answer_words:
         await  message.channel.send('Пропиши в чат команаду !help')
     if msg in goodbye_words:
@@ -217,9 +153,5 @@ async def quote(ctx):
 async def max(ctx):
     anecdote = get_anecdote()
     await ctx.send(anecdote)
-
-# @client.command(aliases = cards_words) TODO доделать
-# async def cerd_user(ctx):
-
 
 client.run(settings['token'])
